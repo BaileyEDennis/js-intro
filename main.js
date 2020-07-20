520ce8207efcef07fc3cbd1dd99dd2357ba84dc6
David Dennis <david.dc.dennis@gmail.com>
2020-07-20 18:52:15 -0500

    tested data and primitive types

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("New code, who dis?")
+// undefined
+//let x; 
+//console.log(x);
+
+//booleans
+// let x = true;
+// let y = false;
+//console.log(x, y);
+
+// if (y) {
+    // console.log("this is true!")
+// } else {
+    // console.log("this is false!");
+// }
+
+// Numbers
+// let number = '22';
+
+// console.log(Number(number) + 22);
+
+// Strings
+// let string = 'Today is Monday!';
+
+// if (string === 'today is monday') {
+//     console.log("YAYYYYAA!!!!")
+// }
+
+// console.log(typeof(string));
+
+let array = []
+console.log(typeof array);
